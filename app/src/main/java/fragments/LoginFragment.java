97d3604e0f578ccd2963ebbb60e85174b5222eb9
Administrator <gharesagar80@gmail.com>
2019-01-21 17:06:41 +0530

    final design

--- app/src/main/java/fragments/LoginFragment.java
+++ app/src/main/java/fragments/LoginFragment.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 import com.example.sagar.loginsinupdesigns.R;
 
@@ -18,13 +21,27 @@ public class LoginFragment extends Fragment {
     public LoginFragment() {
         // Required empty public constructor
     }
+    View view;
+
+    RelativeLayout layout1,layout2;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view=inflater.inflate(R.layout.test, container, false);
+
+        layout1=view.findViewById(R.id.layout1);
+        layout2=view.findViewById(R.id.layout2);
+
+        final Animation slide_in_up = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_in_up);
+        final Animation slide_in_down = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_in_down);
+
+        layout1.startAnimation(slide_in_down);
+        layout2.startAnimation(slide_in_up);
+
+        return view;
     }
 
 }
--- app/src/main/java/fragments/SignupFragment.java
+++ app/src/main/java/fragments/SignupFragment.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 import com.example.sagar.loginsinupdesigns.R;
 
@@ -18,13 +21,26 @@ public class SignupFragment extends Fragment {
     public SignupFragment() {
         // Required empty public constructor
     }
+    View view;
+    RelativeLayout layout1,layout2;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        view= inflater.inflate(R.layout.test1, container, false);
+
+        layout1=view.findViewById(R.id.layout1);
+        layout2=view.findViewById(R.id.layout2);
+
+        final Animation slide_in_up = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_in_up);
+        final Animation slide_in_down = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_in_down);
+
+        layout1.startAnimation(slide_in_down);
+        layout2.startAnimation(slide_in_up);
+
+        return view;
     }
 
 }
